3eb4ac0cc94d8bc402ec0a7eea9eec76edb21227
Kohei Arai <tobecomebig@gmail.com>
2016-12-08 22:37:04 -0500

    Started working on lexer

---
+++ 1_lex/lexer.c
@@ -0,0 +1,12 @@
+#import <stdio.h>
+#import <stdlib.h>
+
+#define DEFAULT_INPUT_FILE "input.pl0"
+#define FILE_NAME_WITHOUT_COMMENT "source_without_comment.pl0"
+
+int shouldPrintSource = 0;
+int shouldPrintSourceWithoutComment = 0;
+
+int main(int argc, char** argv) {
+    
+}
