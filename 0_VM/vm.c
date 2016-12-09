19ca3efff39a66eb359b8afb3267c267a6aa8891
Kohei Arai <tobecomebig@gmail.com>
2016-12-08 19:48:17 -0500

    Implemented printing PL/0 code

--- 0_VM/vm.c
+++ 0_VM/vm.c
@@ -30,6 +30,13 @@ int ir = 0;
 // Function declarations
 int readInstructions(char* inputFileName);
 void printInstructions();
+void printAssemblyCode();
+char* getAssemblerCode(struct instruction instr);
+char* getOpPart(struct instruction instr);
+char* getLPart(char* opPart, struct instruction instr);
+char* getMPart(char* opPart, struct instruction instr);
+
+
 
 int main(int argc, char** argv) {
     int i = 0;
@@ -49,6 +56,7 @@ int main(int argc, char** argv) {
         return 0;
     }
     
+    printAssemblyCode();
 }
 
 // Save each instructions into instructions. 
@@ -63,7 +71,8 @@ int readInstructions(char* inputFileName) {
 
     char* line;
     int op, l, m;
-    while (fscanf(input, "%d %d %d\n", &op, &l, &m) != EOF) {
+    // Read instruction till the end of the file. If the number of instruction goes over MAX_CODE_LENGTH, don't store the rest of instructions.
+    while (fscanf(input, "%d %d %d\n", &op, &l, &m) != EOF && numInstructions < MAX_CODE_LENGTH) {
         struct instruction newInstruction;
         newInstruction.op = op;
         newInstruction.l = l;
@@ -86,6 +95,108 @@ void printInstructions() {
         printf("%d %d %d\n", instr.op, instr.l, instr.m);
     }
 }
+
+void printAssemblyCode() {
+    printf("PL/0 code:\n\n");
+    
+    int i;
+    for (i = 0; i < numInstructions; i++) {
+        printf("%3d  %s\n", i, getAssemblerCode(instructions[i]));
+    }
+}
+
+char* getAssemblerCode(struct instruction instr) {
+    char* opPart = getOpPart(instr);
+    char* lPart  = getLPart(opPart, instr);
+    char* mPart  = getMPart(opPart, instr);
+    
+    char* returnS = malloc(sizeof(char) * (strlen(opPart) + strlen(lPart) + strlen(mPart) + 2));
+    sprintf(returnS, "%s%s%s  ", opPart, lPart, mPart);
+    return returnS;
+}
+
+// With given instruction, return 3 assembly action code.
+char* getOpPart(struct instruction instr) {
+    int op = instr.op;
+    int m = instr.m;
+    
+    if      (op == 1) return "LIT";
+    else if (op == 2) {
+        if      (m == 0) return "RET";
+        else if (m == 1) return "NEG";
+        else if (m == 2) return "ADD";
+        else if (m == 3) return "SUB";
+        else if (m == 4) return "MUL";
+        else if (m == 5) return "DIV";
+        else if (m == 6) return "ODD";
+        else if (m == 7) return "MOD";
+        else if (m == 8) return "EQL";
+        else if (m == 9) return "NEQ";
+        else if (m == 10) return "LSS";
+        else if (m == 11) return "LEQ";
+        else if (m == 12) return "GTR";
+        else if (m == 13) return "GEQ";
+    }
+    else if (op == 3) return "LOD";
+    else if (op == 4) return "STO";
+    else if (op == 5) return "CAL";
+    else if (op == 6) return "INC";
+    else if (op == 7) return "JMP";
+    else if (op == 8) return "JPC";
+    else if (op == 9) {
+        if      (m == 0) return "OUT";
+        else if (m == 1) return "INP";
+        else if (m == 2) return "HLT";
+    }
+
+    // It should never reaches here. When it reaches here, something weird is going on
+    printf("Something wrong is happening.\n");
+    return "";
+}
+
+char* getLPart(char* opPart, struct instruction instr) {
+    // Handle opPart is not defined
+    if (strcmp(opPart, "") == 0) return "";
+    
+    int i;
+    int len = 3;
+    char* showL[] = {"LOD", "STO", "CAL"};
+    
+    for (i = 0; i < len; i++) {
+        if (strcmp(opPart, showL[i]) == 0) {
+            char* lPart = malloc(sizeof(char) * 5);
+            sprintf(lPart, "%5d", instr.l);
+            return lPart;
+        }
+    }
+    
+    return "     ";
+}
+
+char* getMPart(char* opPart, struct instruction instr) {
+    // Handle opPart is not defined
+    if (strcmp(opPart, "") == 0) return "";
+
+    int i;
+    int len = 2;
+    char* hideM[] = {"RET", "HLT"};
+    
+    for (i = 0; i < len; i++) {
+        if (strcmp(opPart, hideM[i]) == 0) {
+            return "     ";
+        }
+    }
+    
+    char* mPart = malloc(sizeof(char) * 5);
+    sprintf(mPart, "%5d", instr.m);
+    return mPart;
+}
+
+
+
+
+
+
 
 
 
